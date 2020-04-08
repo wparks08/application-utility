43c1e196bec27f2c953ee5d8b0715126b8a6e2a1
Will Parks <wparks08@gmail.com>
2020-04-07 23:18:03 -0700

    BREAKING CHANGE: prep for Electron rewrite
    removed all old JavaFX files

--- src/AppUtility/mapping/Conditional.java
+++
@@ -1,6 +0,0 @@
-package AppUtility.mapping;
-
-public enum Conditional {
-    EQUALS { public String toString() { return "Equals"; }},
-    CONTAINS { public String toString() { return "Contains"; }}
-}

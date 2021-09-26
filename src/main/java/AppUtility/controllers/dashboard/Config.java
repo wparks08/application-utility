9c7e3442daee0ccd90d011bb1411d1146d782133
Will Parks <wparks08@gmail.com>
2021-09-26 08:59:48 -0700

    refactor: separate constants

--- src/main/java/AppUtility/controllers/dashboard/Config.java
+++ src/main/java/AppUtility/controllers/dashboard/Config.java
@@ -1,8 +1,10 @@
 package AppUtility.controllers.dashboard;
 
-public class Config {
+public final class Config {
+    public static final String FORM_MAPPING_FXML_PATH = "/formMapping.fxml";
+    public static final String FORM_GENERATION_FXML_PATH = "/formGeneration.fxml";
+    public static final String MAPPING_MENU_BUTTON_TEXT = "Form Mapping";
+    public static final String GENERATION_MENU_BUTTON_TEXT = "Form Generation";
 
     private Config() {}
-
-
 }
